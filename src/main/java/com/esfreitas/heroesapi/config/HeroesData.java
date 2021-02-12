3ed724144bec6edaf5e5acb6b9eefd764f16cff8
Elton Freitas <eltons.freitas01@gmail.com>
2021-02-12 09:24:13 -0300

    adicionando arquivos HeroesData e HeroesConstant

--- src/main/java/com/esfreitas/heroesapi/config/HeroesData.java
+++ src/main/java/com/esfreitas/heroesapi/config/HeroesData.java
@@ -7,34 +7,27 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
-import com.amazonaws.services.dynamodbv2.model.KeyType;
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 import static com.esfreitas.heroesapi.constans.HeroesConstant.ENDPOINT_DYNAMO;
 import static com.esfreitas.heroesapi.constans.HeroesConstant.REGION_DYNAMO;
 
-
-public class HeroesTable {
+public class HeroesData {
     public static void main(String [] args) throws  Exception{
-
         AmazonDynamoDB client = AmazonDynamoDBAsyncClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
                 .build();
         DynamoDB dynamoDB = new DynamoDB(client);
 
-        String tableName = "Heroes_Table";
+        Table table = dynamoDB.getTable("Heroes_Table");
+        Item hero = new Item()
+                .withPrimaryKey("id", 1)
+                .withString("name", "Mulher Maravilha")
+                .withString("universe", "dc comics")
+                .withNumber("films", 3);
 
-        try {
-            Table table = dynamoDB.createTable(tableName,
-                    Arrays.asList(new KeySchemaElement("id", KeyType.HASH)),
-                    Arrays.asList(new AttributeDefinition("id", ScalarAttributeType.S)),
-                    new ProvisionedThroughput(5L, 5l));
-                    table.waitForActive();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        PutItemOutcome outcome = table.putItem(hero);
+
     }
 }
